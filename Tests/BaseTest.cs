9bc848585c738c142efbd577bd2b65c401274b58
Ben Hutchison <ben@aldaviva.com>
2022-06-29 01:20:50 -0700

    Tweaking magic test timeouts in an effort to make tests pass on GitHub Actions hosted runners (doubled global WAIT_TIME)

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -4,7 +4,7 @@ namespace Tests {
 
     public abstract class BaseTest {
 
-        protected static readonly TimeSpan WAIT_TIME = TimeSpan.FromMilliseconds(100);
+        protected static readonly TimeSpan WAIT_TIME = TimeSpan.FromMilliseconds(200);
 
         protected int executionCount     = 0;
         protected int mostRecentArgument = -1;
